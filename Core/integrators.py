a80172f19fadba890746300b916aa7626f6baeeb
Xavier Koch <xavier.koch07@gmail.com>
2026-05-05 21:57:05 -0400

    added rk4 step for greater accuracy

--- Core/integrators.py
+++ Core/integrators.py
@@ -13,3 +13,30 @@ def leapfrog_step(state, dt):
     state.position += state.velocity * dt
     compute_forces(state)
     state.velocity += state.acceleration * (dt / 2)  
+
+def rk4_step(state, dt):
+    s = state.copy()
+    compute_forces(s)
+    k1_velocity = s.acceleration
+    k1_position = state.velocity
+
+    s2 = state.copy()
+    s2.position = state.position + k1_position * dt/2
+    compute_forces(s2)
+    k2_velocity = s2.acceleration
+    k2_position = state.velocity + k1_velocity * dt/2
+
+    s3 = state.copy()
+    s3.position = state.position + k2_position * dt/2
+    compute_forces(s3)
+    k3_velocity = s3.acceleration
+    k3_position = state.velocity + k2_velocity * dt/2
+
+    s4 = state.copy()
+    s4.position = state.position + k3_position * dt
+    compute_forces(s4)
+    k4_velocity = s4.acceleration
+    k4_position = state.velocity + k3_velocity * dt
+
+    state.position += (k1_position + 2*k2_position + 2*k3_position + k4_position) * dt/6
+    state.velocity += (k1_velocity + 2*k2_velocity + 2*k3_velocity + k4_velocity) * dt/6
